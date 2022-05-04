52c75299564482f32a471c1f36a7a45903d4c0ec
Elfadel <mame12fallou93@outlook.com>
2022-05-04 13:00:28 +0000

    login done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+   <div class="row  justify-content-center align-items-center">
+      <div class="col-sm-4  col-sm-offset-4"><!--pour centrer le form-->
+         <form class="centrer" action="login" method="POST">
+  		   <div class="form-group">
+                 @csrf
+    		 <label for="exampleInputEmail1" class="form-label">Email address</label>
+   			 <input type="email" name ="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  	       </div>
+           <div class="form-group">
+    		<label for="exampleInputPassword1" class="form-label">Password</label>
+    		<input type="password" name="password" class="form-control" id="exampleInputPassword1">
+  	       </div><br>
+
+  		<button type="submit" class="btn btn-primary">Login</button>
+       </form>
+
+     </div>
+    </div>
+</div>
+@endsection
